59db17b3408b65255c7994735cd96e8a4d19f097
bestlice123-commits <219862931+giovanazoker@users.noreply.github.com>
2025-10-09 09:09:31 -0300

    criando exercicios

---
+++ alicia/005.py
@@ -0,0 +1,6 @@
+
+familia = ["Deibson", "Aline", "lohane", "Alicya", "Valentina", "Ryan", ]
+
+
+familia_ordenada = sorted(familia)
+print(familia_ordenada)
---
+++ sabriny/005.py
@@ -0,0 +1,6 @@
+
+familia = ["joaqui" , "raquel" , "sabrina" , "alice" , "robert"]
+
+
+familia_ordenada = sorted(familia)
+print(familia_ordenada)
