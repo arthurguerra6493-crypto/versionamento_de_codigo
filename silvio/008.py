1120eb2c42da14b20fb39de93c1e4aa12e9de09d
vitoria212 <226084625+henrrique199713-png@users.noreply.github.com>
2025-10-29 08:02:18 -0300

    r

---
+++ silvio/008.py
@@ -0,0 +1,22 @@
+cidades = []
+
+print("Digite os nomes das cidades ou digite 'sair' para encerrar:")
+
+
+while True:
+   
+    nome_cidade = input("Digite o nome da cidade: ")
+    
+   
+    if nome_cidade.lower() == 'sair':
+        print("Programa Encerrado..........")
+        break
+    else:
+      
+        cidades.append(nome_cidade)
+
+
+print("\nCidades digitadas:")
+for cidade in cidades:
+    print(f"- {cidade}")
+
